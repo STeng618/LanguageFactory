19f805f305cb5ccd4de06536f1b741950d4304db
STeng618 <shantengyap@gmail.com>
2025-07-05 16:11:16 +0800

    Fix incorrect breaking char type

--- include/parser.hpp
+++ include/parser.hpp
@@ -9,8 +9,8 @@ namespace Langfact {
 
     class Parser {
         private: 
-        static const std::set<std::string_view> BREAKING_CHARS; 
-
+        static const std::set<char> BREAKING_CHARS; 
+        
         public: 
         static std::unique_ptr<Token> parse(std::string_view expr);
     };
