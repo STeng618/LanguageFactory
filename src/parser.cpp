5f1d08027297294f1bba020f2a23be67b971eac1
STeng618 <shantengyap@gmail.com>
2025-07-05 16:22:38 +0800

    Add % to breaking chars

--- src/parser.cpp
+++ src/parser.cpp
@@ -14,7 +14,7 @@
 using namespace Langfact;
 
 const std::set<char> Parser::BREAKING_CHARS = {
-    '+', '-', '*', '/', '&', '>', '<', '=', '!', ':','^', '(', ')', ',', ' '
+    '+', '-', '*', '/', '&', '>', '<', '=', '!', ':','^', '(', ')', ',', ' ', '%'
 }; 
 
 template <typename T>
