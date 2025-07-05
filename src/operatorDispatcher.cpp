944a86e07d234fad5b655c524d87a7370bc59660
STeng618 <shantengyap@gmail.com>
2025-07-05 16:22:11 +0800

    Use y/n based on whether there is a prior token

--- src/operatorDispatcher.cpp
+++ src/operatorDispatcher.cpp
@@ -5,52 +5,52 @@
 using namespace Langfact; 
 
 const std::unordered_map<std::string_view, std::function<std::unique_ptr<Operator>(Token::ChildrenList&&)>> OperatorDispatcher::OP_MAP = {
-    {"b+", [](Langfact::Token::ChildrenList&& children) {
+    {"y+", [](Langfact::Token::ChildrenList&& children) {
         return std::make_unique<Langfact::OpSum>(std::move(children));
     }},
-    {"b-", [](Langfact::Token::ChildrenList&& children) {
+    {"y-", [](Langfact::Token::ChildrenList&& children) {
         return std::make_unique<Langfact::OpSubtract>(std::move(children));
     }},
-    {"b*", [](Langfact::Token::ChildrenList&& children) {
+    {"y*", [](Langfact::Token::ChildrenList&& children) {
         return std::make_unique<Langfact::OpMultiply>(std::move(children));
     }},
-    {"b/", [](Langfact::Token::ChildrenList&& children) {
+    {"y/", [](Langfact::Token::ChildrenList&& children) {
         return std::make_unique<Langfact::OpDivide>(std::move(children));
     }},
-    {"b=", [](Langfact::Token::ChildrenList&& children) {
+    {"y=", [](Langfact::Token::ChildrenList&& children) {
         return std::make_unique<Langfact::OpEQ>(std::move(children));
     }},
-    {"b<>", [](Langfact::Token::ChildrenList&& children) {
+    {"y<>", [](Langfact::Token::ChildrenList&& children) {
         return std::make_unique<Langfact::OpNE>(std::move(children));
     }},
-    {"b>", [](Langfact::Token::ChildrenList&& children) {
+    {"y>", [](Langfact::Token::ChildrenList&& children) {
         return std::make_unique<Langfact::OpGT>(std::move(children));
     }},
-    {"b>=", [](Langfact::Token::ChildrenList&& children) {
+    {"y>=", [](Langfact::Token::ChildrenList&& children) {
         return std::make_unique<Langfact::OpGE>(std::move(children));
     }},
-    {"b<", [](Langfact::Token::ChildrenList&& children) {
+    {"y<", [](Langfact::Token::ChildrenList&& children) {
         return std::make_unique<Langfact::OpLT>(std::move(children));
     }},
-    {"b<=", [](Langfact::Token::ChildrenList&& children) {
+    {"y<=", [](Langfact::Token::ChildrenList&& children) {
         return std::make_unique<Langfact::OpLE>(std::move(children));
     }},
-    {"b&", [](Langfact::Token::ChildrenList&& children) {
+    {"y&", [](Langfact::Token::ChildrenList&& children) {
         return std::make_unique<Langfact::OpConcat>(std::move(children));
     }},
-    {"b!", [](Langfact::Token::ChildrenList&& children) {
+    {"y!", [](Langfact::Token::ChildrenList&& children) {
         return std::make_unique<Langfact::OpSheetRef>(std::move(children));
     }},
-    {"b:", [](Langfact::Token::ChildrenList&& children) {
+    {"y:", [](Langfact::Token::ChildrenList&& children) {
         return std::make_unique<Langfact::OpRangeSeparator>(std::move(children));
     }},
-    {"b^", [](Langfact::Token::ChildrenList&& children) {
+    {"y^", [](Langfact::Token::ChildrenList&& children) {
         return std::make_unique<Langfact::OpExponentiate>(std::move(children));
     }},
     {"n-", [](Langfact::Token::ChildrenList&& children) {
         return std::make_unique<Langfact::OpNegate>(std::move(children));
     }},
-    {"u+", [](Langfact::Token::ChildrenList&& children) {
+    {"n+", [](Langfact::Token::ChildrenList&& children) {
         return std::make_unique<Langfact::OpUnaryPlus>(std::move(children));
     }},
     {"y%", [](Langfact::Token::ChildrenList&& children) {
@@ -61,9 +61,9 @@ const std::unordered_map<std::string_view, std::function<std::unique_ptr<Operato
 const int OperatorDispatcher::MAX_OP_LEN = 2;
 
 std::pair<std::unique_ptr<Operator>,int> OperatorDispatcher::dispatch(
-    std::string_view name, bool has_token_since_last_nullptr, Token::ChildrenList&& children
+    std::string_view name, bool has_a_prior_token, Token::ChildrenList&& children
 ) {
-    std::string formatted = (has_token_since_last_nullptr ? std::string("b") : std::string("u")) + std::string(name);
+    std::string formatted = (has_a_prior_token ? std::string("y") : std::string("n")) + std::string(name);
     for (int len = std::min(static_cast<int>(name.size()), OperatorDispatcher::MAX_OP_LEN); len > 0; len--) {
         auto it = OperatorDispatcher::OP_MAP.find(formatted.substr(0, len + 1)); 
         if (it != OperatorDispatcher::OP_MAP.end()) {
