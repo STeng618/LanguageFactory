839ff32e5df5dde2cb0324f8f270a3dde50d5fe4
STeng618 <shantengyap@gmail.com>
2025-07-07 21:19:31 +0800

    Make dimensions public const and remove defaults

--- include/value/cellReference.hpp
+++ include/value/cellReference.hpp
@@ -9,17 +9,16 @@
 namespace Langfact {
     class CellReference : public Token {
         public:
+        const std::string m_repr;
+        const int m_l, m_t, m_r, m_b;
+
         static std::unique_ptr<CellReference> create(std::string_view expr);
-        std::pair<int,int> get_dimensions();
         void identify() const override {
             std::cout << "<CellReference: " << m_repr << " l:" << m_l << " t:" << m_t  << " r:" << m_r << " b:" << m_b << ">";
         }
 
         private:
-        std::string m_repr;
-        int m_l, m_t, m_r, m_b;
-
-        CellReference(std::string_view repr, int l = -1, int t = -1, int r = -1, int b = -1)
+        CellReference(std::string_view repr, int l, int t, int r, int b)
         : m_repr(repr), m_l(l), m_t(t), m_r(r), m_b(b) {}
     };
 }
