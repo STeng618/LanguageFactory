ec551c935c5cf6a5cb83eb7901a08f304878baf4
STeng618 <shantengyap@gmail.com>
2025-07-11 00:36:17 +0800

    Optimisation: remove unordered_map and use hash

--- src/operatorDispatcher.cpp
+++ src/operatorDispatcher.cpp
@@ -3,75 +3,96 @@
 #include <formula/operator.hpp>
 #include <arena.hpp>
 
-using namespace Langfact; 
+using namespace Langfact;
 
-const std::unordered_map<std::string_view, std::function<Operator*(Token::ChildrenList&&)>> OperatorDispatcher::OP_MAP = {
-    {"y+", [](Langfact::Token::ChildrenList&& children) {
-        return Arena<Langfact::OpSum>::get_instance().create(std::move(children));
-    }},
-    {"y-", [](Langfact::Token::ChildrenList&& children) {
-        return Arena<Langfact::OpSubtract>::get_instance().create(std::move(children));
-    }},
-    {"y*", [](Langfact::Token::ChildrenList&& children) {
-        return Arena<Langfact::OpMultiply>::get_instance().create(std::move(children));
-    }},
-    {"y/", [](Langfact::Token::ChildrenList&& children) {
-        return Arena<Langfact::OpDivide>::get_instance().create(std::move(children));
-    }},
-    {"y=", [](Langfact::Token::ChildrenList&& children) {
-        return Arena<Langfact::OpEQ>::get_instance().create(std::move(children));
-    }},
-    {"y<>", [](Langfact::Token::ChildrenList&& children) {
-        return Arena<Langfact::OpNE>::get_instance().create(std::move(children));
-    }},
-    {"y>", [](Langfact::Token::ChildrenList&& children) {
-        return Arena<Langfact::OpGT>::get_instance().create(std::move(children));
-    }},
-    {"y>=", [](Langfact::Token::ChildrenList&& children) {
-        return Arena<Langfact::OpGE>::get_instance().create(std::move(children));
-    }},
-    {"y<", [](Langfact::Token::ChildrenList&& children) {
-        return Arena<Langfact::OpLT>::get_instance().create(std::move(children));
-    }},
-    {"y<=", [](Langfact::Token::ChildrenList&& children) {
-        return Arena<Langfact::OpLE>::get_instance().create(std::move(children));
-    }},
-    {"y&", [](Langfact::Token::ChildrenList&& children) {
-        return Arena<Langfact::OpConcat>::get_instance().create(std::move(children));
-    }},
-    {"y!", [](Langfact::Token::ChildrenList&& children) {
-        return Arena<Langfact::OpSheetRef>::get_instance().create(std::move(children));
-    }},
-    // {"y:", [](Langfact::Token::ChildrenList&& children) {
-    //     return Arena<Langfact::OpRangeSeparator>::get_instance().create(std::move(children));
-    // }},
-    {"y^", [](Langfact::Token::ChildrenList&& children) {
-        return Arena<Langfact::OpExponentiate>::get_instance().create(std::move(children));
-    }},
-    {"n-", [](Langfact::Token::ChildrenList&& children) {
-        return Arena<Langfact::OpNegate>::get_instance().create(std::move(children));
-    }},
-    {"n+", [](Langfact::Token::ChildrenList&& children) {
-        return Arena<Langfact::OpUnaryPlus>::get_instance().create(std::move(children));
-    }},
-    {"y%", [](Langfact::Token::ChildrenList&& children) {
-        return Arena<Langfact::OpPercent>::get_instance().create(std::move(children));
-    }}
+template <size_t N>
+constexpr uint32_t hash(const char (&str)[N]) {
+    uint32_t hash = 2166136261u;
+    for (size_t i = 0; i < N - 1; i++) { // Last char assumed to be '\0' 
+        hash ^= static_cast<unsigned char>(str[i]);
+        hash *= 16777619u;
+    }
+    return hash;
+}
+
+constexpr uint32_t h_plus  = hash("+");
+constexpr uint32_t h_minus = hash("-");
+constexpr uint32_t h_star  = hash("*");
+constexpr uint32_t h_slash = hash("/");
+constexpr uint32_t h_eq    = hash("=");
+constexpr uint32_t h_neq   = hash("<>");
+constexpr uint32_t h_gt    = hash(">");
+constexpr uint32_t h_gte   = hash(">=");
+constexpr uint32_t h_lt    = hash("<");
+constexpr uint32_t h_lte   = hash("<=");
+constexpr uint32_t h_amp   = hash("&");
+constexpr uint32_t h_excl  = hash("!");
+constexpr uint32_t h_caret = hash("^");
+constexpr uint32_t h_percent = hash("%");
+
+
+template <size_t N>
+constexpr bool are_unique_hashes(const std::array<uint32_t, N>& hashes) {
+    for (size_t i = 0; i < N; i++) {
+        for (size_t j = i + 1; j < N; j++) {
+            if (hashes[i] == hashes[j]) return false;
+        }
+    }
+    return true;
+}
+
+constexpr std::array<uint32_t, 14> op_hashes = {
+    h_plus, h_minus, h_star, h_slash, h_eq, h_neq,
+    h_gt, h_gte, h_lt, h_lte, h_amp, h_excl, h_caret, h_percent
 };
 
+static_assert(
+    are_unique_hashes(op_hashes), 
+    "Hash collision detected among operators"
+);
 
 const int OperatorDispatcher::MAX_OP_LEN = 2;
 
-std::pair<Operator*,int> OperatorDispatcher::dispatch(
+std::pair<Operator*, int> OperatorDispatcher::dispatch(
     std::string_view name, bool has_a_prior_token, Token::ChildrenList&& children
 ) {
-    std::string formatted = (has_a_prior_token ? std::string("y") : std::string("n")) + std::string(name);
-    for (int len = std::min(static_cast<int>(name.size()), OperatorDispatcher::MAX_OP_LEN); len > 0; len--) {
-        auto it = OperatorDispatcher::OP_MAP.find(formatted.substr(0, len + 1)); 
-        if (it != OperatorDispatcher::OP_MAP.end()) {
-            return {it->second(std::move(children)), len};
+    if (name.empty()) return {nullptr, 0};
+
+    uint32_t op_hash = 0;
+    if (name.size() >= 2) {
+        const char two_char[] = {name[0], name[1], '\0'};
+        op_hash = hash(two_char);
+        if (has_a_prior_token) {
+            switch (op_hash) {
+                case h_neq: return {Arena<OpNE>::get_instance().create(std::move(children)), 2};
+                case h_gte: return {Arena<OpGE>::get_instance().create(std::move(children)), 2};
+                case h_lte: return {Arena<OpLE>::get_instance().create(std::move(children)), 2};
+            }
         }
     }
+
+    const char one_char[] = {name[0], '\0'};
+    op_hash = hash(one_char);
+    if (has_a_prior_token) {
+        switch (op_hash) {
+            case h_plus:     return {Arena<OpSum>::get_instance().create(std::move(children)), 1};
+            case h_minus:    return {Arena<OpSubtract>::get_instance().create(std::move(children)), 1};
+            case h_star:     return {Arena<OpMultiply>::get_instance().create(std::move(children)), 1};
+            case h_slash:    return {Arena<OpDivide>::get_instance().create(std::move(children)), 1};
+            case h_eq:       return {Arena<OpEQ>::get_instance().create(std::move(children)), 1};
+            case h_gt:       return {Arena<OpGT>::get_instance().create(std::move(children)), 1};
+            case h_lt:       return {Arena<OpLT>::get_instance().create(std::move(children)), 1};
+            case h_amp:      return {Arena<OpConcat>::get_instance().create(std::move(children)), 1};
+            case h_excl:     return {Arena<OpSheetRef>::get_instance().create(std::move(children)), 1};
+            case h_caret:    return {Arena<OpExponentiate>::get_instance().create(std::move(children)), 1};
+            case h_percent:  return {Arena<OpPercent>::get_instance().create(std::move(children)), 1};
+        }
+    } else {
+        switch (op_hash) {
+            case h_minus: return {Arena<OpNegate>::get_instance().create(std::move(children)), 1};
+            case h_plus:  return {Arena<OpUnaryPlus>::get_instance().create(std::move(children)), 1};
+        }
+    }
+
     return {nullptr, 0};
 }
-
