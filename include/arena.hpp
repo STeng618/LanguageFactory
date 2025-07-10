ff416b5b676d2ae3dee756772d07e628a7a01c1b
STeng618 <shantengyap@gmail.com>
2025-07-10 22:51:03 +0800

    Optimisation: remove virtual calls

--- include/arena.hpp
+++ include/arena.hpp
@@ -5,9 +5,12 @@
 #include <vector>
 
 class ArenaBase {
-public:
-    virtual void clear() = 0;
-    virtual ~ArenaBase() = default;
+    protected:
+    size_t m_offset;
+
+    public:
+    ArenaBase(): m_offset(0) {};
+    void clear() {  m_offset = 0; }
 };
 
 void register_arena(ArenaBase* arena);
@@ -15,41 +18,36 @@ void clear_all_arenas();
 
 template <typename T, size_t N = 500>
 class Arena : public ArenaBase {
-public:
-    void clear() override {
-        m_offset = 0;
-    }
 
-    T* reserve() {
-        if (m_offset >= N) {
-            throw std::runtime_error("Arena has reached capacity");
+    private:
+        std::byte m_buffer[N][sizeof(T)];
+        Arena() : ArenaBase() {};
+
+        Arena(const Arena&) = delete;
+        Arena& operator=(const Arena&) = delete;
+        Arena(Arena&&) = delete;
+        Arena& operator=(Arena&&) = delete;
+
+    public:
+        T* reserve() {
+            if (m_offset >= N) {
+                throw std::runtime_error("Arena has reached capacity");
+            }
+            return reinterpret_cast<T*>(&m_buffer[m_offset++]);
         }
-        return reinterpret_cast<T*>(&m_buffer[m_offset++]);
-    }
 
-    template <typename... Args>
-    T* create(Args&&... args) {
-        T* ptr = reserve();
-        return new (ptr) T(std::forward<Args>(args)...); 
-    }
+        template <typename... Args>
+        T* create(Args&&... args) {
+            T* ptr = reserve();
+            return new (ptr) T(std::forward<Args>(args)...); 
+        }
 
-    static Arena& get_instance() {
-        static Arena arena_instance;
-        static bool is_registered = (register_arena(&arena_instance), true);
-        (void)(is_registered);
-        return arena_instance;
-    }
-
-private:
-    std::byte m_buffer[N][sizeof(T)];
-    size_t m_offset;
-
-    Arena() : m_offset(0) {}
-
-    Arena(const Arena&) = delete;
-    Arena& operator=(const Arena&) = delete;
-    Arena(Arena&&) = delete;
-    Arena& operator=(Arena&&) = delete;
+        static Arena& get_instance() {
+            static Arena arena_instance;
+            static bool is_registered = (register_arena(&arena_instance), true);
+            (void)(is_registered);
+            return arena_instance;
+        }
 };
 
 #endif
